a27b547a32ca2f13a6e467656332496b7664d805
unknown <amine.naitsiahmed@gmail.com>
2024-12-14 23:03:00 +0100

    tester les competances sur 4 unit

--- src/controller/player_handler.py
+++ src/controller/player_handler.py
@@ -4,17 +4,18 @@ from src.controller.animation_manager import AnimationManager
 from src.model.unit import Unit
 from src.view.map import Map
 
-
 class PlayerHandler:
 
-    def __init__(self, player: Unit, animation_manager: AnimationManager, map: Map):
-        self.name = player.name
+    def __init__(self, players, animation_manager: AnimationManager, map: Map):
+        self.players = players
         self.animation_manager = animation_manager
         self.map = map
+        self.current_turn = 'Player 1'
+        self.current_hero_index = 0
 
     @property
-    def player(self):
-        return self.animation_manager.heros[self.name]
+    def current_player(self):
+        return self.players[self.current_turn][self.current_hero_index]
 
     def get_effect(self, type):
         if type in self.animation_manager.effects:
@@ -23,50 +24,85 @@ class PlayerHandler:
 
     def key_pressed_event(self):
         pressed = pygame.key.get_pressed()
-        anim = self.animation_manager.get_animation(self.name)
-        effect = self.animation_manager.get_effect(self.name)
-        # print(f"Effect active: {effect.effect_image is not None}")
+        anim = self.animation_manager.get_animation(self.current_player.name)
+        effect = self.animation_manager.get_effect(self.current_player.name)
+
         if effect is not None and effect.current_effect is None:
             if pressed[pygame.K_LEFT]:
-                self.animation_manager.heros[self.name].move(-self.animation_manager.heros[self.name].speed, 0, anim.rect, anim.feet, self.map)
-                self.animation_manager.heros[self.name].set_state('movement', 'side', None)
-                self.animation_manager.update_animation(self.animation_manager.heros[self.name], 'movement', 'side')
-                self.animation_manager.set_orientation(self.name, 'left')
-                self.animation_manager.heros[self.name].reset_movement_range()
+                self.current_player.move(-self.current_player.speed, 0, anim.rect, anim.feet, self.map)
+                self.current_player.set_state('movement', 'side', None)
+                self.animation_manager.update_animation(self.current_player, 'movement', 'side')
+                self.animation_manager.set_orientation(self.current_player.name, 'left')
                 return 'side'
             if pressed[pygame.K_RIGHT]:
-                self.animation_manager.heros[self.name].move(+self.animation_manager.heros[self.name].speed, 0, anim.rect, anim.feet, self.map)
-                self.animation_manager.heros[self.name].set_state('movement', 'side', None)
-                self.animation_manager.update_animation(self.animation_manager.heros[self.name], 'movement', 'side')
-                self.animation_manager.set_orientation(self.name, 'right')
-                self.animation_manager.heros[self.name].reset_movement_range()
+                self.current_player.move(+self.current_player.speed, 0, anim.rect, anim.feet, self.map)
+                self.current_player.set_state('movement', 'side', None)
+                self.animation_manager.update_animation(self.current_player, 'movement', 'side')
+                self.animation_manager.set_orientation(self.current_player.name, 'right')
                 return 'side'
             if pressed[pygame.K_UP]:
-                self.animation_manager.heros[self.name].move(0, -self.animation_manager.heros[self.name].speed, anim.rect, anim.feet, self.map)
-                self.animation_manager.heros[self.name].set_state('movement', 'up-down', None)
-                self.animation_manager.update_animation(self.animation_manager.heros[self.name], 'movement', 'up-down')
-                self.animation_manager.heros[self.name].reset_movement_range()
+                self.current_player.move(0, -self.current_player.speed, anim.rect, anim.feet, self.map)
+                self.current_player.set_state('movement', 'up-down', None)
+                self.animation_manager.update_animation(self.current_player, 'movement', 'up-down')
                 return 'up-down'
             if pressed[pygame.K_DOWN]:
-                self.animation_manager.heros[self.name].move(0, +self.animation_manager.heros[self.name].speed, anim.rect, anim.feet, self.map)
-                self.animation_manager.heros[self.name].set_state('movement', 'up-down', None)
-                self.animation_manager.update_animation(self.animation_manager.heros[self.name], 'movement', 'up-down')
-                self.animation_manager.heros[self.name].reset_movement_range()
+                self.current_player.move(0, +self.current_player.speed, anim.rect, anim.feet, self.map)
+                self.current_player.set_state('movement', 'up-down', None)
+                self.animation_manager.update_animation(self.current_player, 'movement', 'up-down')
                 return 'up-down'
             else:
                 if not pressed[pygame.K_a] and not pressed[pygame.K_d]:
-                    self.animation_manager.heros[self.name].set_state('idle', 'idle', None)
-                    self.animation_manager.update_animation(self.animation_manager.heros[self.name], 'idle', 'idle')
+                    self.current_player.set_state('idle', 'idle', None)
+                    self.animation_manager.update_animation(self.current_player, 'idle', 'idle')
         return 'idle'
 
     def key_down_event(self, event, screen, dt):
-        if event.key == pygame.K_a:
-            if dt is not None:
-                type = "Diamond shield"
-                print(f"Setting state to 'attacks' with type {type} at position ({self.animation_manager.heros[self.name].x}, {self.animation_manager.heros[self.name].y})")
-                self.animation_manager.heros[self.name].set_state('defenses', type, self.animation_manager.get_effect(self.animation_manager.heros[self.name].name), (self.animation_manager.heros[self.name].x + 200, self.animation_manager.heros[self.name].y))
-                print(self.animation_manager.get_effect(self.animation_manager.heros[self.name].name).effect_x)
-                self.animation_manager.update(self.animation_manager.heros[self.name], 'defenses', type, dt, screen)
-        elif event.key == pygame.K_d:
-            self.animation_manager.heros[self.name].set_state('dead', 'dead', None)
-            self.animation_manager.update_animation(self.animation_manager.heros[self.name], 'dead', 'dead')
+        if self.current_turn == 'Player 1':
+            if event.key == pygame.K_r:
+                self.perform_attack(self.current_player, 0)
+            elif event.key == pygame.K_t:
+                self.perform_attack(self.current_player, 1)
+        elif self.current_turn == 'Player 2':
+            if event.key == pygame.K_r:
+                self.perform_attack(self.current_player, 0)
+            elif event.key == pygame.K_t:
+                self.perform_attack(self.current_player, 1)
+
+    def perform_attack(self, attacker, attack_index):
+        target = self.get_opponent_hero(attacker)
+        if target:
+            attack = attacker.competences['attacks'][attack_index]
+            if attack.is_within_range((attacker.x, attacker.y), (target.x, target.y)):
+                attack.activate(attacker, target, self.animation_manager.heros.values())
+                self.switch_turn()
+            else:
+                print(f"{target.name} est hors de portée de {attacker.name}.")
+        else:
+            print(f"Aucune cible valide pour {attacker.name}.")
+
+    def get_opponent_hero(self, attacker):
+        opponents = []
+        if self.current_turn == 'Player 1':
+            opponents = self.players['Player 2']
+        else:
+            opponents = self.players['Player 1']
+
+        for opponent in opponents:
+            attack = attacker.competences['attacks'][0]  # Utiliser la première attaque pour vérifier la portée
+            if attack.is_within_range((attacker.x, attacker.y), (opponent.x, opponent.y)):
+                return opponent
+        return None
+
+    def switch_turn(self):
+        if self.current_turn == 'Player 1':
+            if self.current_hero_index + 1 < len(self.players['Player 1']):
+                self.current_hero_index += 1
+            else:
+                self.current_turn = 'Player 2'
+                self.current_hero_index = 0
+        else:
+            if self.current_hero_index + 1 < len(self.players['Player 2']):
+                self.current_hero_index += 1
+            else:
+                self.current_turn = 'Player 1'
+                self.current_hero_index = 0
--- tests/test_animation.py
+++ tests/test_animation.py
@@ -1,85 +1,80 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import pygame
 
 from src.controller.animation_manager import AnimationManager
-# from src.controller.game_handler import AVATAR_NAMES, selected_avatar
-from src.controller.menu_handler import MenuHandler
 from src.controller.player_handler import PlayerHandler
 from src.model.hero_factory import HeroFactory
-from src.model.player import Player
-from src.settings import SingletonMeta, Settings
+from src.settings import SingletonMeta
 from src.view.animation_factory import AnimationFactory
 from src.view.map import Map
 from src.view.screen import Screen
-
 
 class Game(metaclass=SingletonMeta):
 
     def __init__(self):
         self.is_running = False
-        self.players = {}
+        self.players = {'Player 1': [], 'Player 2': []}  # Dictionnaire pour les personnages des joueurs
         self.screen = Screen()
         self.map = Map(self.screen)
+        self.map.map_layer.zoom = 2
         self.clock = pygame.time.Clock()
         self.animation_manager = AnimationManager()
         self.animation_factory = AnimationFactory()
-        self.selected_player = None
-        self.settings = Settings()
+
+        # Créer les personnages pour les joueurs
+        self.players['Player 1'].append(HeroFactory.erza(250, 50, 'Player 1 - Erza')[0])  # Personnage 1 du joueur 1
+        self.players['Player 1'].append(HeroFactory.gray(250, 150, 'Player 1 - Gray')[0])  # Personnage 2 du joueur 1
+        self.players['Player 2'].append(HeroFactory.natsu(700, 300, 'Player 2 - Natsu')[0])  # Personnage 1 du joueur 2
+        self.players['Player 2'].append(HeroFactory.kansuke(700, 450, 'Player 2 - Kansuke')[0])  # Personnage 2 du joueur 2
+
+        # Ajouter les personnages à l'animation manager
+        for player in self.players['Player 1'] + self.players['Player 2']:
+            self.animation_manager.add_hero(player)
+
+        # Ajouter les animations et effets pour chaque personnage
+        for player in self.players['Player 1'] + self.players['Player 2']:
+            sprite_conf = HeroFactory.sprite_config(player.name)
+            self.animation_manager.add_animation(player.name, self.animation_factory.create_animation(player, sprite_conf))
+            self.animation_manager.add_effect(player.name, self.animation_factory.create_effect(sprite_conf))
+
+        # Définir l'orientation des personnages
+        for player in self.players['Player 1']:
+            self.animation_manager.set_orientation(player.name, 'right')
+        for player in self.players['Player 2']:
+            self.animation_manager.set_orientation(player.name, 'left')
 
     def update(self):
-        unit = self.selected_player.selected_unit
+        # Mise à jour des éléments du jeu (collisions, etc.)
         self.map.group.update()
-        if unit is not None:
-            anim = self.animation_manager.get_animation(unit.name)
+
+        # Animation et mouvement des personnages
+        for player in self.players['Player 1'] + self.players['Player 2']:
+            anim = self.animation_manager.get_animation(player.name)
             if anim.feet.collidelist(self.map.collisions) > -1:
-                unit.move_back(anim.rect, anim.feet)
-        self.map.update(self.animation_manager) # ajouté
+                player.move_back(anim.rect, anim.feet)
+
+        # Mise à jour de la carte
+        self.map.update(self.animation_manager)
 
     def run(self):
-        SCREEN_WIDTH, SCREEN_HEIGHT = 1600, 900  # Fullscreen resolution
-        DISPLAY_WIDTH = 1200  # Width for the avatars
-        DISPLAY_START_Y = 650  # Starting Y position for avatars
-        AVATAR_SIZE = 120  # Size of each avatar
-        INFO_BOX_HEIGHT = SCREEN_HEIGHT - (DISPLAY_START_Y + AVATAR_SIZE + 20)  # Remaining space for info
-        AVATAR_SPACING = 20
-        start_x = (SCREEN_WIDTH - DISPLAY_WIDTH) // 2
-        font = pygame.font.Font(None, 36)
-        selected_avatars = []
-
-        AVATAR_NAMES = list(self.selected_player.units.keys())
-        selected_avatar = None
-
-        info_start_y = DISPLAY_START_Y + AVATAR_SIZE + 20
-        info_box = pygame.Rect(start_x, info_start_y, DISPLAY_WIDTH, INFO_BOX_HEIGHT)
-
-        avatars = []
-        i = 0
-        for i in range(3):
-            avatar = pygame.Rect(start_x + i * (AVATAR_SIZE + AVATAR_SPACING), DISPLAY_START_Y, AVATAR_SIZE,
-                                 AVATAR_SIZE)
-            avatars.append(avatar)
-
         # Initialisation du gestionnaire de joueur
-        handler = PlayerHandler(self.selected_player.selected_unit, self.animation_manager, self.map)
+        handler = PlayerHandler(self.players, self.animation_manager, self.map)
         self.is_running = True
 
         while self.is_running:
-            self.screen.display.fill((0, 0, 0))  # Black background
-
             # Calcul du delta time pour synchroniser les mises à jour
             dt = self.clock.tick(60) / 1000
 
-            # # Gestion des événements utilisateur
+            # Gestion des événements utilisateur
             for event in pygame.event.get():
                 if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_END:  # Fin du jeu
+                    if event.key == pygame.K_DELETE:  # Fin du jeu
                         self.is_running = False
                     handler.key_down_event(event, self.screen.display, dt)
-
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    for i, rect in enumerate(avatars):
-                        if (rect not in selected_avatars) and rect.collidepoint(event.pos):
-                            selected_avatar = AVATAR_NAMES[i]
-                            selected_avatars.append(selected_avatar)
 
             # Gestion continue des touches pressées
             handler.key_pressed_event()
@@ -88,142 +83,36 @@ class Game(metaclass=SingletonMeta):
             self.update()
 
             # Dessin et rendu de l'écran
-            self.draw(avatars, selected_avatar, AVATAR_NAMES, info_box)
+            self.draw()
 
         pygame.quit()
         print(self.map.width, self.map.height)
 
-    def draw(self, avatar_rects, selected_avatar, avatar_names, info_box):
+    def draw(self):
+        # Dessin de l'animation
         dt = self.clock.tick(60) / 1000
-        animation = self.animation_manager.get_animation(self.selected_player.selected_unit.name)
-        effect = self.animation_manager.get_effect(self.selected_player.selected_unit.name)
 
-        # Mise à jour
-        if animation.update(dt, self.animation_manager.orientation[self.selected_player.selected_unit.name]) or (effect and effect.current_effect is not None) :
-            # if effect and effect.current_effect is not None:
-            if effect and not effect.apply_effect(dt, self.animation_manager.orientation[self.selected_player.selected_unit.name]):
-                print("Effect animation completed.")
-                effect.current_effect = None  # Réinitialiser l'effet une fois terminé
+        # Mise à jour de l'animation et des effets
+        for player in self.players['Player 1'] + self.players['Player 2']:
+            animation = self.animation_manager.get_animation(player.name)
+            effect = self.animation_manager.get_effect(player.name)
+            if animation.update(dt, self.animation_manager.orientation[player.name]) or (effect and effect.current_effect is not None):
+                if effect and not effect.apply_effect(dt, self.animation_manager.orientation[player.name]):
+                    print(f"Effect animation completed for {player.name}.")
+                    effect.current_effect = None
 
         # Rendu (affichage)
         self.map.update(self.animation_manager)
 
-        # Dessiner l'effet
-        if effect and effect.current_effect is not None:
-            # print(f"Drawing effect at index {effect.effect_index}")
-            effect.draw(self.screen.display)
+        # Dessiner les effets
+        for player in self.players['Player 1'] + self.players['Player 2']:
+            effect = self.animation_manager.get_effect(player.name)
+            if effect and effect.current_effect is not None:
+                effect.draw(self.screen.display)
 
-        for i, rect in enumerate(avatar_rects):
-            self.screen.display.blit(pygame.image.load(f"../media/UI/{avatar_names[i]}_hud.png"), rect)
-            if selected_avatar == avatar_names[i]:
-                self.selected_player.selected_unit = self.selected_player.units[selected_avatar]
-                self.selected_player.selected_unit.is_selected = True
-                self.screen.display.blit(pygame.image.load(f"../media/UI/selected_hud.png"),
-                            avatar_rects[i])  # Highlight selected avatar
-
-        # Draw Info Section
-        pygame.draw.rect(self.screen.display, (50, 50, 50), info_box)
-        pygame.draw.rect(self.screen.display, (200, 200, 200), info_box, 2)  # Border
-
-        # self.draw_walkable_overlay(self.animation_manager.get_animation(self.selected_player.selected_unit.name).get_walkable_tiles(self.selected_player.selected_unit.movement_range))
-
+        # Mettre à jour l'écran
         pygame.display.flip()
 
-    def initialize_entities(self, selected_units):
-        if selected_units is not None:
-            i = 0
-            for name in selected_units.keys():
-                player = Player(name)
-                j = 0
-                for unit_name in selected_units[name]:
-                    unit, sprite_conf = HeroFactory.create_hero(unit_name, self.settings.team_indexes[i][j][0], self.settings.team_indexes[i][j][1], name)
-                    if unit is not None:
-                        if j == 0:
-                            unit.is_selected = True
-                        player.units[unit_name] = unit
-                        self.animation_manager.add_hero(unit)
-                        self.animation_manager.add_animation(unit.name, self.animation_factory.create_animation(unit, sprite_conf))
-                        self.animation_manager.add_effect(unit.name, self.animation_factory.create_effect(sprite_conf))
-                        self.animation_manager.set_orientation(unit.name, 'right')
-                        j += 1
-                self.players[name] = player
-                i += 1
-
-    def start(self):
-        pygame.init()
-
-        # Initialisation
-        is_running = True
-        pressed = False
-
-        # Configuration
-        icon = pygame.image.load('../media/UI/icon.png')
-        banner = pygame.image.load('../media/UI/banner.png')
-        pygame.display.set_icon(icon)
-        pygame.display.set_caption("Strategy Game")
-        screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)  # Résolution explicite pour tester
-        banner = pygame.transform.scale(banner, screen.get_size())
-
-        # handler = PlayerHandler(self.selected_player.selected_unit, self.animation_manager, self.map)
-        clock = pygame.time.Clock()
-        menu_handler = MenuHandler()
-        menu_handler.menu(pressed, banner, clock, screen)
-        self.initialize_entities(menu_handler.selected_units)
-        print(self.players)
-        self.selected_player = self.players[list(menu_handler.selected_units.keys())[0]]
-        print(self.selected_player)
-        self.selected_player.select_unit()
-        self.run()
-        # for name, unit_dict in self.players.items():
-        #     for unit_name, unit in unit_dict.items():
-        #         print(name, unit_name, unit)
-        pygame.quit()
-
-    # TODO je suis ici
-    def handle_turn(self, handler, avatar_rects, selected_avatar, avatar_names, info_box):
-        for unit in self.selected_player.units.values():
-            has_acted = False
-            self.selected_player.selected_unit = unit
-            self.selected_player.selected_unit.is_selected = True
-            self.update()
-            self.draw(avatar_rects, selected_avatar, avatar_names, info_box)
-
-            while not has_acted:
-                for event in pygame.event.get():
-                    # Calcul du delta time pour synchroniser les mises à jour
-                    dt = self.clock.tick(60) / 1000
-
-                    # # Gestion des événements utilisateur
-                    for event in pygame.event.get():
-                        if event.type == pygame.KEYDOWN:
-                            if event.key == pygame.K_END:  # Fin du jeu
-                                self.is_running = False
-                            elif event.key == pygame.K_1:
-                                self.selected_player.selected_unit.attack()
-                            handler.name = self.selected_player.selected_unit.name
-                            handler.key_down_event(event, self.screen.display, dt)
-
-                    # Gestion continue des touches pressées
-                    handler.key_pressed_event()
-
-                    # Mise à jour des éléments du jeu
-                    self.update()
-
-                    # Dessin et rendu de l'écran
-                    self.draw(avatar_rects, selected_avatar, avatar_names, info_box)
-
-    def draw_walkable_overlay(self, walkable_tiles):
-        for x, y in walkable_tiles:
-            tile_x = x * self.settings.tile_width * self.map.zoom_factor
-            tile_y = y * self.settings.tile_height * self.map.zoom_factor
-            overlay_color = (0, 255, 0, 128)  # Vert semi-transparent
-            overlay_surface = pygame.Surface(
-                (int(self.settings.tile_width * self.map.zoom_factor), int(self.settings.tile_height * self.map.zoom_factor)), pygame.SRCALPHA
-            )
-            overlay_surface.fill(overlay_color)
-            self.screen.display.blit(overlay_surface, (tile_x, tile_y))
-
-
 game = Game()
-# print(game.screen.get_size())
-game.start()
+print(game.screen.get_size())
+game.run()
